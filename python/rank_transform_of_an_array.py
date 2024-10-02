e1a6992e51919e1d2f0d2a21c06f6b4f3aa90ab3
vubom01 <lehuyvu1852001@gmail.com>
2024-10-02 17:16:12 +0700

    daily question

---
+++ python/rank_transform_of_an_array.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/rank-transform-of-an-array/?envType=daily-question&envId=2024-10-02
+
+from typing import List
+
+
+class Solution:
+    def arrayRankTransform(self, arr: List[int]) -> List[int]:
+        sorted_unique = sorted(set(arr))
+        rank_dict = {value: index + 1 for index, value in enumerate(sorted_unique)}
+        return [rank_dict[value] for value in arr]
+
+
+sol = Solution()
+print(sol.arrayRankTransform([40,10,20,30]))

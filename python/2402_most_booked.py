850cae357eefbae0774cab8cd5d3ed5597205c9f
vubom01 <lehuyvu1852001@gmail.com>
2025-07-14 16:43:54 +0700

    most booked solution

--- python/2402_most_booked.py
+++ python/2402_most_booked.py
@@ -1,4 +1,20 @@
-# https://leetcode.com/problems/meeting-rooms-iii
+"""
+https://leetcode.com/problems/meeting-rooms-iii/description/
+"""
+
+"""
+- Sort the meetings list in increasing order based on start time
+- Use 2 to keep track of free rooms and active rooms in increasing order based on end time
+- Loop through meeting
+- Push all active rooms whose end time is earlier than the meeting's start time into the list of free rooms
+- If there is a free room, assign it to the meeting 
+- If not, assign the meeting to the active room that finishes first
+- Update active rooms list
+- Increment the counter for the room that was used
+- TC: O(n log n)
+- SC: O(n)
+"""
+
 import heapq
 from typing import List
 

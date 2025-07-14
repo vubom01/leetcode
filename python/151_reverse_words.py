aeb182827df9bcfef6bd9520023382d047aab2c8
vubom01 <lehuyvu1852001@gmail.com>
2025-07-14 16:55:31 +0700

    reverse words

--- python/151_reverse_words.py
+++ python/151_reverse_words.py
@@ -1,4 +1,7 @@
-# https://leetcode.com/problems/reverse-words-in-a-string/?envType=study-plan-v2&envId=top-interview-150
+"""
+https://leetcode.com/problems/reverse-words-in-a-string/description/
+"""
+
 
 class Solution:
     def reverseWords(self, s: str) -> str:

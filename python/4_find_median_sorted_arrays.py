bfe6f302fb1422c2bb8ea572324c17b1775e475e
vubom01 <lehuyvu1852001@gmail.com>
2025-07-14 16:21:59 +0700

    find median sorted array solution

--- python/4_find_median_sorted_arrays.py
+++ python/4_find_median_sorted_arrays.py
@@ -1,4 +1,27 @@
-# https://leetcode.com/problems/median-of-two-sorted-arrays/
+"""
+https://leetcode.com/problems/median-of-two-sorted-arrays/description/
+"""
+
+"""
+- Approach 1: Create new array from nums1 and nums2, sort the new array and return result
+  -- TC: O((n+m) log (n+m))
+  -- SC: O(n+m)
+- Approach 2: Using merge sort to merge 2 arrays
+  -- TC: O(n+m)
+  -- SC: O(n+m) -> can be optimize to O(1)
+- Approach 3:
+  -- To find the median, we need to divide the new array into 2 parts of equal length
+  -- Part 1: [0...mid1] of nums1 and [0...mid2] of nums2
+  -- Part 2: [mid1+1...n-1] of nums1 and [mid2+1...m-1] of nums2
+  -- nums1[mid1] <= nums2[mid2+1] and nums2[mid2] <= nums1[mid1+1]
+  -- mid2 = (n+m+1) // 2 - mid1
+  -- We need to find mid1
+  -- Use binary search to find mid1
+  -- If nums1[mid1] > nums2[mid2+1], we need to decrease mid1 to increase mid2
+  -- Otherwise, we need to increase mid1 to decrease mid2
+  -- TC: O(log min(n,m))
+  -- SC: O(1)
+"""
 
 from typing import List
 

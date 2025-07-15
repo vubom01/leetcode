53fa28af7964e6137059ee07e1c9595b2c6cd035
vubom01 <lehuyvu1852001@gmail.com>
2025-07-15 11:18:35 +0700

    take characters solution

--- python/2516_take_characteres.py
+++ python/2516_take_characteres.py
@@ -1,4 +1,14 @@
-# https://leetcode.com/problems/take-k-of-each-character-from-left-and-right/?envType=daily-question&envId=2024-11-20
+"""
+https://leetcode.com/problems/take-k-of-each-character-from-left-and-right/description/
+"""
+
+"""
+- Count the frequency of each character
+- Use 2 pointers to keep track the removable segment and find the minimum possible result
+- TC: O(n)
+- SC: O(1)
+"""
+
 
 class Solution:
     def takeCharacters(self, s: str, k: int) -> int:
@@ -21,5 +31,6 @@ class Solution:
             result = min(result, left - 1 + n - right)
             right += 1
         return result
+
 
 print(Solution().takeCharacters('ababaaccaa', 0))
